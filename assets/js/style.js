04335ed808cc8ef1d8b17250c563bace22576ffa
shotflix <78502195+shotflix@users.noreply.github.com>
2021-02-06 22:12:46 -0300

    Add files via upload

--- assets/js/style.js
+++ assets/js/style.js
@@ -1,12 +1,36 @@
 const buscar = document.getElementById('buscar')
 const iconBuscar = document.getElementById('iconBuscar')
 const form = document.getElementById('form')
+const iconMenu = document.getElementById('iconMenu')
+const removeMenu = document.getElementById('removeMenu')
+const nav = document.getElementById('nav')
+const inputBuscar = document.getElementById('buscar')
+const header = document.querySelector('header')
+const ulHeader = document.getElementById('ul-header')
+
+var time;
+
+
 
 form.addEventListener('mouseenter', () => {
-    (function aumentarTamanho(){
-        buscar.style.height = '40px'
-        buscar.style.width = '250px'
+    (function LimparTimeOut() {
+        clearTimeout(time);
     })();
+})
+
+form.addEventListener('click', () => {
+
+    if (visualViewport.width < 855) {
+        (function aumentarTamanho(){
+            buscar.style.height = '40px'
+            buscar.style.width = '100%'
+        })();
+    } else {
+        (function aumentarTamanho(){
+            buscar.style.height = '40px'
+            buscar.style.width = '250px'
+        })();
+    }
 
     (function esconderIcon(){
         iconBuscar.style.display = 'none'
@@ -14,13 +38,52 @@ form.addEventListener('mouseenter', () => {
 })
 
 form.addEventListener('mouseleave', () => {
-    (function diminuirTamanho(){
-        buscar.style.height = '0px'
-        buscar.style.width = '0px'
+    time = setTimeout(() => {
+        (function diminuirTamanho(){
+            buscar.style.height = '0px'
+            buscar.style.width = '0px'
+        })();
+    
+        (function mostrarIcon(){
+            iconBuscar.style.display = 'block'
+        })();
+    }, 2000);
+})
+
+
+iconMenu.addEventListener('click', () => {
+    (function mostrarNav() {
+        nav.style.width = '50%'
+        form.style.display = 'flex'
+        ulHeader.style.display = 'block'
     })();
 
-    (function mostrarIcon(){
-        iconBuscar.style.display = 'block'
+    (function removeIconMenu() {
+        iconMenu.style.display = 'none'
+    })();
+
+    (function mostrarIconRemoveMenu() {
+        removeMenu.style.display = 'block'
     })();
 })
 
+removeMenu.addEventListener('click', () => {
+    (function mostrarIconMenu() {
+        removeMenu.style.display = 'none'
+        iconMenu.style.display = 'block'
+    })();
+
+    (function removerNavMobile() {
+        nav.style.width = '0'
+        ulHeader.style.display = 'none'
+        form.style.display = 'none'
+    })();
+})
+
+
+iconBuscar.addEventListener('click', () => {
+    inputBuscar.focus()
+    if (visualViewport.width < 855 && inputBuscar.focus) {
+        inputBuscar.style.margin = '10px 5px'
+    }
+})
